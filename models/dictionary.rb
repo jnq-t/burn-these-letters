66a1dce8ac5f54e8c317577862f834b8f19ee6f1
Jesse Quebbeman-Turley <jesse.quebbeman@mx.com>
2024-09-05 00:56:41 -0700

    hookup the dictionary class

--- models/dictionary.rb
+++ models/dictionary.rb
@@ -1,7 +1,73 @@
-module ORM
-  module Modules
-    class Dictionary
+module Models
+  class Dictionary
+    ##
+    # files
+    require_relative '../orm/dsl.rb'
+
+    ##
+    # dependencies
+    require 'active_support/inflector'
+
+    ##
+    # class methods
+
+    def self.find
+      ## returns a given dictionary
+    end
+
+    def initialize(name:)
+      @table_name = name
+    end
+
+    ##
+    # I/O
+
+    ##
+    # DSL methods (should probably be included)
+    def values
+      {"verbs": ["go", "do", "be"], "nouns": ["cats", "dogs"] }
+    end
+
+    # TODO add better pluralize
+    def model_dir_name
+      self.class.name.split("::").last.split(/(?=[A-Z])/).join("_").downcase.pluralize
+    end
+
+    def table_name
+      "Ariel"
+    end
+
+    def message
+      "trying out metadata"
+    end
+
+    def save
+      ::Orm::Dsl::Interface.new(model_instance: self).save
+    end
+
+
+    ##
+    # TODO
+
+    def import_yml
+      # imports entire yml file
+    end
+
+    def add_grouping
 
     end
+
+    def add_key
+
+    end
+
+    def add_value
+
+    end
+
+    def keys
+
+    end
+
   end
 end
--- orm/dsl.rb
+++ orm/dsl.rb
@@ -1,107 +1,97 @@
 module Orm
   class Dsl
-    require "json"
-    require "yaml"
+    class Interface
+      require "json"
+      require "yaml"
 
-    DB_VERSION = "0.0.1"
+      DB_VERSION = "0.0.1"
+      #
+      # def self.find
+      # end
 
-    class GenericModel
-      def values
-        {"verbs": ["go", "do", "be"], "nouns": ["cats", "dogs"] }
+
+      def initialize(model_instance: GenericModel.new)
+        @model_instance = model_instance
       end
 
-      # TODO add better pluralize
-      def model_dir_name
-        self.class.name.split("::").last.split(/(?=[A-Z])/).join("_").downcase + "s"
+      attr_reader :model_instance
+
+      def save
+        ensure_file_structure
+        write_files
       end
 
-      def table_name
-        "Ariel"
+      def find
+        path_to_table_dir
       end
 
-      def message
-        "trying out metadata"
-      end
-    end
+      private
 
-    def initialize(model_instance: GenericModel.new)
-      @model_instance = model_instance
-    end
-
-    attr_reader :model_instance
-
-    def save
-      ensure_file_structure
-      write_files
-    end
-
-    def find(table_name)
-      path_to_table_dir
-    end
-
-  private
-
-    def ensure_file_structure
-      create_db_dir
-      create_model_dir
-      create_table_dir
-      create_backup_dir
-    end
-
-    ##
-    # file paths
-
-    def path_to_model_dir
-      "./db/#{model_instance.model_dir_name}"
-    end
-
-    def path_to_table_dir
-      "#{path_to_model_dir}/#{model_instance.table_name.downcase}"
-    end
-
-
-    ##
-    # file structure
-
-    def create_db_dir
-      Dir.mkdir("./db") unless Dir.exist?("./db")
-    end
-
-    def create_model_dir
-      Dir.mkdir(path_to_model_dir) unless Dir.exist?(path_to_model_dir)
-    end
-    
-    def create_table_dir
-      Dir.mkdir(path_to_table_dir) unless Dir.exist?(path_to_table_dir)
-    end
-    
-    def create_backup_dir
-      path = "#{path_to_table_dir}/backups"
-      Dir.mkdir(path) unless Dir.exist?(path)
-    end
-
-    ##
-    # I/O
-
-    def write_files
-      headers = {
-        "metadata" =>
-          {
-            :db_version => DB_VERSION,
-            :message => model_instance.message
-          }
-      }
-      # overwrite main file
-      values = headers.merge model_instance.values
-      File.open("#{path_to_table_dir}/#{model_instance.table_name}.yml", 'w') do |file|
-        file.write(values.to_yaml)
+      def ensure_file_structure
+        create_db_dir
+        create_model_dir
+        create_table_dir
+        create_backup_dir
       end
 
-      # make backup
-      # TODO add a check here to see if the file is literally identical, and if so to not make another backup
-      digest = Time.now.to_i
-      File.open("#{path_to_table_dir}/backups/#{digest}.yml", 'w') do |file|
-        file.write(values.to_yaml)
+      ##
+      # file paths
+
+      def path_to_model_dir
+        "./db/#{model_instance.model_dir_name}"
+      end
+
+      def path_to_table_dir
+        "#{path_to_model_dir}/#{model_instance.table_name.downcase}"
+      end
+
+      def load_path
+        "#{path_to_table_dir}/#{model_instance.table_name.downcase}.yml"
+      end
+
+      ##
+      # file structure
+
+      def create_db_dir
+        Dir.mkdir("./db") unless Dir.exist?("./db")
+      end
+
+      def create_model_dir
+        Dir.mkdir(path_to_model_dir) unless Dir.exist?(path_to_model_dir)
+      end
+
+      def create_table_dir
+        Dir.mkdir(path_to_table_dir) unless Dir.exist?(path_to_table_dir)
+      end
+
+      def create_backup_dir
+        path = "#{path_to_table_dir}/backups"
+        Dir.mkdir(path) unless Dir.exist?(path)
+      end
+
+      ##
+      # I/O
+
+      def write_files
+        headers = {
+          "metadata" =>
+            {
+              :db_version => DB_VERSION,
+              :message => model_instance.message
+            }
+        }
+        # overwrite main file
+        values = headers.merge model_instance.values
+        File.open("#{path_to_table_dir}/#{model_instance.table_name}.yml", 'w') do |file|
+          file.write(values.to_yaml)
+        end
+
+        # make backup
+        # TODO add a check here to see if the file is literally identical, and if so to not make another backup
+        digest = Time.now.to_i
+        File.open("#{path_to_table_dir}/backups/#{digest}.yml", 'w') do |file|
+          file.write(values.to_yaml)
+        end
       end
     end
   end
